c00be12602dd0e3c7128463c929e246fb2a66d55
Luigi Carvalho <carvalholuigi25@gmail.com>
2023-10-31 13:45:44 +0000

    debug nodeemailer options

--- src/app/[locale]/utils/sendmailUtils.ts
+++ src/app/[locale]/utils/sendmailUtils.ts
@@ -1,8 +1,8 @@
 export async function sendMailUtil(mailData: any) {
   require('dotenv').config();
   let nodemailer = require('nodemailer');
-  
-  const transporter = nodemailer.createTransport({
+  const debugOpt = true;
+  const confopt = {
     host: process.env.EMAIL_HOST,
     port: parseInt(process.env.EMAIL_PORT!.toString()),
     secure: process.env.EMAIL_IS_SSL === "true" ? true : false,
@@ -10,7 +10,13 @@ export async function sendMailUtil(mailData: any) {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
     }
-  });
+  };
+
+  if(debugOpt == true) {
+    console.log(confopt)
+  }
+  
+  const transporter = nodemailer.createTransport(confopt);
 
   await transporter.sendMail(mailData, (err: any, info: any) => {
     if(err)
