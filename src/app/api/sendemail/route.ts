f07dead897ca16e1ab3eb9a6677e2c0c4afcbbbb
Luigi Carvalho <carvalholuigi25@gmail.com>
2023-11-02 18:02:22 +0000

    added email template for contact form

--- src/app/api/sendemail/route.ts
+++ src/app/api/sendemail/route.ts
@@ -1,14 +1,15 @@
 import { NextResponse } from "next/server";
 import { sendMailUtil } from '../../[locale]/utils/sendmailUtils';
+import { EmailTemplate } from '../../[locale]/templates';
 
 export async function POST(req: any) {  
   const body = await req.json();
   const dataobj = {
     to: process.env.EMAIL_USER,
-    from: body.email,
+    from: `"${body.name}" <${body.email}>`,
     subject: body.subject,
     text: `${body.message}\r\n Sent by ${body.email} (${body.name})`,
-    html: `<!doctype html><html><head><title>Hello</title></head><body><b>${body.message}</b><p>Sent by: ${body.email} (${body.name})</p></body></html>`
+    html: EmailTemplate(body)
   };
 
   console.log(dataobj);
